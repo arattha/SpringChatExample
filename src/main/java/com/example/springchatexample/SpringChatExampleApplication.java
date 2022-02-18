60ece92403c5a29bd4c2ccd7fc4b87637b3b4fc5
최광진 <38902367+arattha@users.noreply.github.com>
2022-02-18 15:40:01 +0900

    Initial commit

---
+++ src/main/java/com/example/springchatexample/SpringChatExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springchatexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springchatexample/SpringChatExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springchatexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
